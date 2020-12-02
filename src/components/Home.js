e082768daa2db1a2c57141532dee820d77a08402
noamschwartz <noamschwartz1@gmail.com>
2020-12-02 19:21:35 +0200

    adding contest-table

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,7 @@ import {
 import { NavLink } from "react-router-dom";
 
 import { getNextFixture } from "../DAL/api/api-football";
+import ContestTable from "./ContestTable";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 import UserAccount from "./UserAccount";
@@ -31,7 +32,7 @@ const Home = (props) => {
     {/* ###########TEMP######### */}
       <Row>
         <Col>
-          <UserAccount/>
+          <ContestTable/>
         </Col>
       </Row>
       {/* ########################## */}
